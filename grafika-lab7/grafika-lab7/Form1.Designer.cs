bcf2220a145513b189e35296fbef15843d6d3f28
patrykkalinowski99 <80587030+patrykkalinowski99@users.noreply.github.com>
2021-05-10 21:40:41 +0200

    Add files via upload

---
+++ grafika-lab7/grafika-lab7/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.bWczytaj = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.obrazek1 = new System.Windows.Forms.PictureBox();
+            this.obrazek2 = new System.Windows.Forms.PictureBox();
+            this.bSkaluj = new System.Windows.Forms.Button();
+            this.wykres1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.wykres2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.bHistogram = new System.Windows.Forms.Button();
+            this.bUsredniajacy = new System.Windows.Forms.Button();
+            this.bGauss = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.obrazek1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obrazek2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wykres1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wykres2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bWczytaj
+            // 
+            this.bWczytaj.BackColor = System.Drawing.Color.Transparent;
+            this.bWczytaj.Location = new System.Drawing.Point(12, 3);
+            this.bWczytaj.Name = "bWczytaj";
+            this.bWczytaj.Size = new System.Drawing.Size(706, 23);
+            this.bWczytaj.TabIndex = 0;
+            this.bWczytaj.Text = "Wczytaj";
+            this.bWczytaj.UseVisualStyleBackColor = false;
+            this.bWczytaj.Click += new System.EventHandler(this.bWczytaj_click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // obrazek1
+            // 
+            this.obrazek1.Location = new System.Drawing.Point(12, 32);
+            this.obrazek1.Name = "obrazek1";
+            this.obrazek1.Size = new System.Drawing.Size(350, 300);
+            this.obrazek1.TabIndex = 1;
+            this.obrazek1.TabStop = false;
+            // 
+            // obrazek2
+            // 
+            this.obrazek2.Location = new System.Drawing.Point(12, 338);
+            this.obrazek2.Name = "obrazek2";
+            this.obrazek2.Size = new System.Drawing.Size(350, 300);
+            this.obrazek2.TabIndex = 2;
+            this.obrazek2.TabStop = false;
+            // 
+            // bSkaluj
+            // 
+            this.bSkaluj.BackColor = System.Drawing.Color.Transparent;
+            this.bSkaluj.Location = new System.Drawing.Point(368, 644);
+            this.bSkaluj.Name = "bSkaluj";
+            this.bSkaluj.Size = new System.Drawing.Size(165, 23);
+            this.bSkaluj.TabIndex = 4;
+            this.bSkaluj.Text = "Skaluj";
+            this.bSkaluj.UseVisualStyleBackColor = false;
+            this.bSkaluj.Click += new System.EventHandler(this.bSkaluj_Click);
+            // 
+            // wykres1
+            // 
+            this.wykres1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.wykres1.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            chartArea3.Name = "ChartArea1";
+            this.wykres1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.wykres1.Legends.Add(legend3);
+            this.wykres1.Location = new System.Drawing.Point(368, 32);
+            this.wykres1.Name = "wykres1";
+            this.wykres1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+            series7.Color = System.Drawing.Color.Red;
+            series7.Legend = "Legend1";
+            series7.Name = "R";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedArea;
+            series8.Color = System.Drawing.Color.Green;
+            series8.Legend = "Legend1";
+            series8.Name = "G";
+            series9.ChartArea = "ChartArea1";
+            series9.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series9.Color = System.Drawing.Color.Blue;
+            series9.Legend = "Legend1";
+            series9.Name = "B";
+            this.wykres1.Series.Add(series7);
+            this.wykres1.Series.Add(series8);
+            this.wykres1.Series.Add(series9);
+            this.wykres1.Size = new System.Drawing.Size(350, 300);
+            this.wykres1.TabIndex = 5;
+            this.wykres1.Text = "chart1";
+            // 
+            // wykres2
+            // 
+            this.wykres2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.wykres2.BorderlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            chartArea4.Name = "ChartArea1";
+            this.wykres2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.wykres2.Legends.Add(legend4);
+            this.wykres2.Location = new System.Drawing.Point(368, 338);
+            this.wykres2.Name = "wykres2";
+            this.wykres2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            series10.ChartArea = "ChartArea1";
+            series10.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+            series10.Color = System.Drawing.Color.Red;
+            series10.Legend = "Legend1";
+            series10.Name = "R";
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StackedArea;
+            series11.Color = System.Drawing.Color.Green;
+            series11.Legend = "Legend1";
+            series11.Name = "G";
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+            series12.Color = System.Drawing.Color.Blue;
+            series12.Legend = "Legend1";
+            series12.Name = "B";
+            this.wykres2.Series.Add(series10);
+            this.wykres2.Series.Add(series11);
+            this.wykres2.Series.Add(series12);
+            this.wykres2.Size = new System.Drawing.Size(350, 300);
+            this.wykres2.TabIndex = 6;
+            this.wykres2.Text = "chart2";
+            // 
+            // bHistogram
+            // 
+            this.bHistogram.BackColor = System.Drawing.Color.Transparent;
+            this.bHistogram.Location = new System.Drawing.Point(12, 644);
+            this.bHistogram.Name = "bHistogram";
+            this.bHistogram.Size = new System.Drawing.Size(172, 23);
+            this.bHistogram.TabIndex = 7;
+            this.bHistogram.Text = "Histogram";
+            this.bHistogram.UseVisualStyleBackColor = false;
+            this.bHistogram.Click += new System.EventHandler(this.bHistogram_Click);
+            // 
+            // bUsredniajacy
+            // 
+            this.bUsredniajacy.BackColor = System.Drawing.Color.Transparent;
+            this.bUsredniajacy.Location = new System.Drawing.Point(190, 644);
+            this.bUsredniajacy.Name = "bUsredniajacy";
+            this.bUsredniajacy.Size = new System.Drawing.Size(172, 23);
+            this.bUsredniajacy.TabIndex = 8;
+            this.bUsredniajacy.Text = "Filtr Uśredniający";
+            this.bUsredniajacy.UseVisualStyleBackColor = false;
+            this.bUsredniajacy.Click += new System.EventHandler(this.bUsredniajacy_Click);
+            // 
+            // bGauss
+            // 
+            this.bGauss.BackColor = System.Drawing.Color.Transparent;
+            this.bGauss.Location = new System.Drawing.Point(539, 644);
+            this.bGauss.Name = "bGauss";
+            this.bGauss.Size = new System.Drawing.Size(179, 23);
+            this.bGauss.TabIndex = 9;
+            this.bGauss.Text = "Filtr Gaussa";
+            this.bGauss.UseVisualStyleBackColor = false;
+            this.bGauss.Click += new System.EventHandler(this.bGauss_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(724, 671);
+            this.Controls.Add(this.bGauss);
+            this.Controls.Add(this.bUsredniajacy);
+            this.Controls.Add(this.bHistogram);
+            this.Controls.Add(this.wykres2);
+            this.Controls.Add(this.wykres1);
+            this.Controls.Add(this.bSkaluj);
+            this.Controls.Add(this.obrazek2);
+            this.Controls.Add(this.obrazek1);
+            this.Controls.Add(this.bWczytaj);
+            this.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.Name = "Form1";
+            this.Text = "Modyfikacja obrazu";
+            ((System.ComponentModel.ISupportInitialize)(this.obrazek1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.obrazek2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wykres1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wykres2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bWczytaj;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox obrazek1;
+        private System.Windows.Forms.PictureBox obrazek2;
+        private System.Windows.Forms.Button bSkaluj;
+        private System.Windows.Forms.DataVisualization.Charting.Chart wykres1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart wykres2;
+        private System.Windows.Forms.Button bHistogram;
+        private System.Windows.Forms.Button bUsredniajacy;
+        private System.Windows.Forms.Button bGauss;
+    }
+}
+
---
+++ grafika-lab7/grafika-lab7/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7
+{
+    public partial class Form1 : Form
+    {
+        private int width, height;
+        int[] red = new int[256];
+        int[] green = new int[256];
+        int[] blue = new int[256];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void bWczytaj_click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                obrazek1.Load(openFileDialog1.FileName);
+                width = obrazek1.Image.Width;
+                height = obrazek1.Image.Height;
+                obrazek2.Image = new Bitmap(width, height);
+            }
+        }
+        private int[] Skaluj(int[] arg)
+        {
+            int min = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                if (arg[i] != 0)
+                {
+                    min = i;
+                    break;
+                }
+            }
+            int max = 255;
+            for (int i = 255; i >= 0; i--)
+            {
+                if (arg[i] != 0)
+                {
+                    max = i;
+                    break;
+                }
+            }
+            int[] wynik = new int[256];
+            double a = 255.0 / (max - min);
+
+            for (int i = 0; i < 256; i++)
+            {
+                wynik[i] = (int)(a * (i - min));
+            }
+            return wynik;
+        }
+        private void bHistogram_Click(object sender, EventArgs e)
+        {
+            Bitmap b1 = (Bitmap)obrazek1.Image;
+            Color kolor;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    kolor = b1.GetPixel(x, y);
+
+                    red[kolor.R]++;
+                    green[kolor.G]++;
+                    blue[kolor.B]++;
+                }
+            }
+            wykres1.Series["R"].Points.Clear();
+            wykres1.Series["G"].Points.Clear();
+            wykres1.Series["B"].Points.Clear();
+
+            for (int i = 0; i < 256; i++)
+            {
+                wykres1.Series["R"].Points.AddXY(i, red[i]);
+                wykres1.Series["G"].Points.AddXY(i, green[i]);
+                wykres1.Series["B"].Points.AddXY(i, blue[i]);
+            }
+            wykres1.Invalidate();
+    }
+        private void bUsredniajacy_Click(object sender, EventArgs e)
+        {
+            Bitmap b1 = (Bitmap)obrazek1.Image;
+            Bitmap b2 = (Bitmap)obrazek2.Image;
+
+            Color k1, k2, k3, k4, k5, k6, k7, k8, k9;
+
+            int m1 = 1;
+            int m2 = 1;
+            int m3 = 1;
+            int m4 = 1;
+            int m5 = 1;
+            int m6 = 1;
+            int m7 = 1;
+            int m8 = 1;
+            int m9 = 1;
+            int maski = m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9;
+
+            if (maski == 0) maski = 1;
+
+            for (int x = 1; x < width - 1; x++)
+            {
+                for (int y = 1; y < height - 1; y++)
+                {
+                    int r, g, b;
+
+                    k1 = b1.GetPixel(x - 1, y - 1);
+                    k2 = b1.GetPixel(x, y - 1);
+                    k3 = b1.GetPixel(x + 1, y - 1);
+                    k4 = b1.GetPixel(x - 1, y);
+                    k5 = b1.GetPixel(x, y);
+                    k6 = b1.GetPixel(x + 1, y);
+                    k7 = b1.GetPixel(x - 1, y + 1);
+                    k8 = b1.GetPixel(x, y + 1);
+                    k9 = b1.GetPixel(x + 1, y + 1);
+
+                    r = k1.R * m1 + k2.R * m2 + k3.R * m3 + k4.R * m4 + k5.R * m5 + k6.R * m6 + k7.R * m7 + k8.R * m8 + k9.R * m9;
+                    r = r / maski;
+
+                    b = k1.B * m1 + k2.B * m2 + k3.B * m3 + k4.B * m4 + k5.B * m5 + k6.B * m6 + k7.B * m7 + k8.B * m8 + k9.B * m9;
+                    b = b / maski;
+
+                    g = k1.G * m1 + k2.G * m2 + k3.G * m3 + k4.G * m4 + k5.G * m5 + k6.G * m6 + k7.G * m7 + k8.G * m8 + k9.G * m9;
+                    g = g / maski;
+
+                    if (r > 255) r = 255;
+                    if (r < 0) r = 0;
+
+                    if (g > 255) g = 255;
+                    if (g < 0) g = 0;
+
+
+                    if (b > 255) b = 255;
+                    if (b < 0) b = 0;
+
+                    b2.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+            obrazek2.Refresh();
+        }
+        private void bGauss_Click(object sender, EventArgs e)
+        {
+            Bitmap b1 = (Bitmap)obrazek1.Image;
+            Bitmap b2 = (Bitmap)obrazek2.Image;
+
+            Color kolor1, kolor2, kolor3, kolor4, kolor5, kolor6, kolor7, kolor8, kolor9;
+
+            int m1 = 1;
+            int m2 = 2;
+            int m3 = 1;
+            int m4 = 2;
+            int m5 = 4;
+            int m6 = 2;
+            int m7 = 1;
+            int m8 = 2;
+            int m9 = 1;
+            int maski = m1 + m2 + m3 + m4 + m5 + m6 + m7 + m8 + m9;
+
+            if (maski == 0) maski = 1;
+
+            for (int x = 1; x < width - 1; x++)
+            {
+                for (int y = 1; y < height - 1; y++)
+                {
+                    int r, g, b;
+
+                    kolor1 = b1.GetPixel(x - 1, y - 1);
+                    kolor2 = b1.GetPixel(x, y - 1);
+                    kolor3 = b1.GetPixel(x + 1, y - 1);
+                    kolor4 = b1.GetPixel(x - 1, y);
+                    kolor5 = b1.GetPixel(x, y);
+                    kolor6 = b1.GetPixel(x + 1, y);
+                    kolor7 = b1.GetPixel(x - 1, y + 1);
+                    kolor8 = b1.GetPixel(x, y + 1);
+                    kolor9 = b1.GetPixel(x + 1, y + 1);
+
+
+                    r = kolor1.R * m1 + kolor2.R * m2 + kolor3.R * m3 + kolor4.R * m4 + kolor5.R * m5 + kolor6.R * m6 + kolor7.R * m7 + kolor8.R * m8 + kolor9.R * m9;
+                    r = r / maski;
+
+                    b = kolor1.B * m1 + kolor2.B * m2 + kolor3.B * m3 + kolor4.B * m4 + kolor5.B * m5 + kolor6.B * m6 + kolor7.B * m7 + kolor8.B * m8 + kolor9.B * m9;
+                    b = b / maski;
+
+
+                    g = kolor1.G * m1 + kolor2.G * m2 + kolor3.G * m3 + kolor4.G * m4 + kolor5.G * m5 + kolor6.G * m6 + kolor7.G * m7 + kolor8.G * m8 + kolor9.G * m9;
+                    g = g / maski;
+
+                    if (r > 255) r = 255;
+                    if (r < 0) r = 0;
+
+                    if (g > 255) g = 255;
+                    if (g < 0) g = 0;
+
+                    if (b > 255) b = 255;
+                    if (b < 0) b = 0;
+                    b2.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+            obrazek2.Refresh();
+        }
+        private void bSkaluj_Click(object sender, EventArgs e)
+        {
+            int[] LUTred = Skaluj(red);
+            int[] LUTgreen = Skaluj(green);
+            int[] LUTblue = Skaluj(blue);
+
+            Bitmap b1 = (Bitmap)obrazek1.Image;
+            Bitmap b2 = (Bitmap)obrazek2.Image;
+
+            int[] r1 = new int[256];
+            int[] g1 = new int[256];
+            int[] bl1 = new int[256];
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Color k = b1.GetPixel(x, y);
+                    Color newk = Color.FromArgb(LUTred[k.R], LUTgreen[k.G], LUTblue[k.B]);
+
+                    b2.SetPixel(x, y, newk);
+
+                    r1[newk.R]++;
+                    g1[newk.G]++;
+                    bl1[newk.B]++;
+                }
+            }
+            obrazek2.Refresh();
+            wykres2.Series["R"].Points.Clear();
+            wykres2.Series["G"].Points.Clear();
+            wykres2.Series["B"].Points.Clear();
+
+            for (int i = 0; i < 256; i++)
+            {
+                wykres2.Series["R"].Points.AddXY(i, r1[i]);
+                wykres2.Series["G"].Points.AddXY(i, g1[i]);
+                wykres2.Series["B"].Points.AddXY(i, bl1[i]);
+            }
+            wykres2.Invalidate();
+        }
+    }
+}
